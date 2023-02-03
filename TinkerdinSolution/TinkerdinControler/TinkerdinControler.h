f9e9acab27b1f9dd9c63342205d69057f79488d4
Oliver Rojas <oliver.rp2003@gmail.com>
2023-02-03 13:29:55 -0500

    Agrege listas Oliver

--- TinkerdinSolution/TinkerdinControler/TinkerdinControler.h
+++ TinkerdinSolution/TinkerdinControler/TinkerdinControler.h
@@ -1,10 +1,30 @@
+//#pragma once
+//
+//using namespace System;
+//
+//namespace TinkerdinControler {
+//	public ref class Class1
+//	{
+//		// TODO: Agregue aquí los métodos de esta clase.
+//	};
+//}
 #pragma once
 
 using namespace System;
+using namespace System::Collections::Generic;
+using namespace TinkerdinModel;
 
 namespace TinkerdinControler {
-	public ref class Class1
-	{
+	public ref class Controller {
+	private:
+		static List<Cliente^>^ clientList;
+		static List<Admin^>^ adminList;
+		static List<Interest^>^ interestList;
+		static List<Course^>^ courseList;
+		static List<Place^>^ placeList;
+
+	public:
 		// TODO: Agregue aquí los métodos de esta clase.
+		//static int AddCli
 	};
 }
