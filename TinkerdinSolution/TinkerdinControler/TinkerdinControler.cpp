b687b88a312d04a931e63aca9603684c857df919
Oliver Rojas <oliver.rp2003@gmail.com>
2023-02-03 16:11:08 -0500

    CRUD Cliente de Oliver

--- TinkerdinSolution/TinkerdinControler/TinkerdinControler.cpp
+++ TinkerdinSolution/TinkerdinControler/TinkerdinControler.cpp
@@ -2,3 +2,55 @@
 
 #include "TinkerdinControler.h"
 
+int TinkerdinControler::Controller::AddClient(Cliente^ client)
+{
+	clientList->Add(client);
+
+	return client->code;
+}
+
+Cliente^ TinkerdinControler::Controller::QueryClientById(int^ clientId)
+{
+	//clientList = (List<Cliente^>^)Persistance::LoadBinary;
+	for (int i = 0; i < clientList->Count; i++)
+		if (clientList[i]->code == clientId)
+			return clientList[i];
+	return nullptr;
+		// TODO: Insertar una instrucción "return" aquí
+}
+
+List<Cliente^>^ TinkerdinControler::Controller::QueryAllClients()
+{
+	//clientList = (List<Cliente^>^)Persistance::LoadBinaryData("clients.bin");
+	return clientList;
+}
+
+int TinkerdinControler::Controller::UpdateClient(Cliente^ client)
+{
+	for (int i = 0; i < clientList->Count; i++)
+	{
+		if (clientList[i]->code == client->code) {
+			clientList[i] = client;
+			//añadir sus persistencia
+			return client->code;
+
+		}
+	}
+	return 0;
+}
+
+int TinkerdinControler::Controller::DeleteClient(int ClientId)
+{	
+	for (int i = 0; i < clientList->Count; i++)
+	{
+		if (clientList[i]->code==ClientId)
+		{
+			clientList->RemoveAt(i);
+			//añadir persisrtencia
+			return ClientId;
+
+		}
+
+	}
+	return 0;
+}
--- TinkerdinSolution/TinkerdinControler/TinkerdinControler.h
+++ TinkerdinSolution/TinkerdinControler/TinkerdinControler.h
@@ -18,13 +18,18 @@ namespace TinkerdinControler {
 	public ref class Controller {
 	private:
 		static List<Cliente^>^ clientList;
-		static List<Admin^>^ adminList;
+		//static List<Admin^>^ adminList;
 		static List<Interest^>^ interestList;
 		static List<Course^>^ courseList;
 		static List<Place^>^ placeList;
 
 	public:
 		// TODO: Agregue aquí los métodos de esta clase.
-		//static int AddCli
+		static int AddClient(Cliente^ client);
+		static Cliente^ QueryClientById(int^ clientId);
+		static List<Cliente^>^ QueryAllClients();
+		static int UpdateClient(Cliente^ client);
+		static int DeleteClient(int ClientId);
+		//static List<Cliente^>^ QueryClientsByNameOrInterest(String^ name, String^ interest);
 	};
 }
