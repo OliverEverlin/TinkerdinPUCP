91af3c0ff6e46f5ea1bab923fb401118423b6b64
Oliver Rojas <oliver.rp2003@gmail.com>
2023-02-03 18:24:26 -0500

    CoursesFormActu del view cpp

--- TinkerdinSolution/TinkerdinView/TinkerdinView.cpp
+++ TinkerdinSolution/TinkerdinView/TinkerdinView.cpp
@@ -1,8 +1,23 @@
 #include "pch.h"
+#include "MainForm.h"
+#include "PlaceForm.h"
+#include "CourseForm.h"
+#include "CalendarForm.h"
 
 using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
 
 int Main(array<System::String ^> ^args)
 {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	TinkerdinView::MainForm form;
+	TinkerdinView::PlaceForm form;
+	TinkerdinView::CourseForm form;
+	TinkerdinView::CalendarForm form;
+	Application::Run(% form);
+	
     return 0;
 }
