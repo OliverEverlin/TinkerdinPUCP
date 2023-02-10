83435e5c265706bef930b74d102422298ff7dda7
KILLERKEVIIINh <a20196417@pucp.edu.pe>
2023-02-10 14:12:30 -0500

    KEV

--- TinkerdinSolution/TinkerdinView/PlaceForm.h
+++ TinkerdinSolution/TinkerdinView/PlaceForm.h
@@ -115,9 +115,10 @@ namespace TinkerdinView {
 			// label1
 			// 
 			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(69, 41);
+			this->label1->Location = System::Drawing::Point(52, 33);
+			this->label1->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(21, 16);
+			this->label1->Size = System::Drawing::Size(19, 13);
 			this->label1->TabIndex = 0;
 			this->label1->Text = L"Id:";
 			this->label1->Click += gcnew System::EventHandler(this, &PlaceForm::label1_Click);
@@ -125,18 +126,20 @@ namespace TinkerdinView {
 			// label2
 			// 
 			this->label2->AutoSize = true;
-			this->label2->Location = System::Drawing::Point(69, 84);
+			this->label2->Location = System::Drawing::Point(52, 68);
+			this->label2->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
 			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(97, 16);
+			this->label2->Size = System::Drawing::Size(75, 13);
 			this->label2->TabIndex = 1;
 			this->label2->Text = L"Disponibilidad:";
 			// 
 			// label3
 			// 
 			this->label3->AutoSize = true;
-			this->label3->Location = System::Drawing::Point(69, 124);
+			this->label3->Location = System::Drawing::Point(52, 101);
+			this->label3->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
 			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(88, 16);
+			this->label3->Size = System::Drawing::Size(71, 13);
 			this->label3->TabIndex = 2;
 			this->label3->Text = L"Participantes:";
 			this->label3->Click += gcnew System::EventHandler(this, &PlaceForm::label3_Click);
@@ -144,18 +147,20 @@ namespace TinkerdinView {
 			// label4
 			// 
 			this->label4->AutoSize = true;
-			this->label4->Location = System::Drawing::Point(69, 164);
+			this->label4->Location = System::Drawing::Point(52, 133);
+			this->label4->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
 			this->label4->Name = L"label4";
-			this->label4->Size = System::Drawing::Size(98, 16);
+			this->label4->Size = System::Drawing::Size(78, 13);
 			this->label4->TabIndex = 3;
 			this->label4->Text = L"Tomacorriente:";
 			// 
 			// label5
 			// 
 			this->label5->AutoSize = true;
-			this->label5->Location = System::Drawing::Point(69, 203);
+			this->label5->Location = System::Drawing::Point(52, 165);
+			this->label5->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
 			this->label5->Name = L"label5";
-			this->label5->Size = System::Drawing::Size(90, 16);
+			this->label5->Size = System::Drawing::Size(72, 13);
 			this->label5->TabIndex = 4;
 			this->label5->Text = L"Tipo de lugar:";
 			this->label5->Click += gcnew System::EventHandler(this, &PlaceForm::label5_Click);
@@ -163,17 +168,19 @@ namespace TinkerdinView {
 			// label6
 			// 
 			this->label6->AutoSize = true;
-			this->label6->Location = System::Drawing::Point(69, 246);
+			this->label6->Location = System::Drawing::Point(52, 200);
+			this->label6->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
 			this->label6->Name = L"label6";
-			this->label6->Size = System::Drawing::Size(65, 16);
+			this->label6->Size = System::Drawing::Size(54, 13);
 			this->label6->TabIndex = 5;
 			this->label6->Text = L"Locación:";
 			// 
 			// btnPlaceAdd
 			// 
-			this->btnPlaceAdd->Location = System::Drawing::Point(413, 60);
+			this->btnPlaceAdd->Location = System::Drawing::Point(310, 49);
+			this->btnPlaceAdd->Margin = System::Windows::Forms::Padding(2);
 			this->btnPlaceAdd->Name = L"btnPlaceAdd";
-			this->btnPlaceAdd->Size = System::Drawing::Size(107, 23);
+			this->btnPlaceAdd->Size = System::Drawing::Size(80, 19);
 			this->btnPlaceAdd->TabIndex = 6;
 			this->btnPlaceAdd->Text = L"Agregar";
 			this->btnPlaceAdd->UseVisualStyleBackColor = true;
@@ -181,9 +188,10 @@ namespace TinkerdinView {
 			// 
 			// btnPlaceUpdate
 			// 
-			this->btnPlaceUpdate->Location = System::Drawing::Point(413, 117);
+			this->btnPlaceUpdate->Location = System::Drawing::Point(310, 95);
+			this->btnPlaceUpdate->Margin = System::Windows::Forms::Padding(2);
 			this->btnPlaceUpdate->Name = L"btnPlaceUpdate";
-			this->btnPlaceUpdate->Size = System::Drawing::Size(111, 23);
+			this->btnPlaceUpdate->Size = System::Drawing::Size(83, 19);
 			this->btnPlaceUpdate->TabIndex = 7;
 			this->btnPlaceUpdate->Text = L"Modificar";
 			this->btnPlaceUpdate->UseVisualStyleBackColor = true;
@@ -191,9 +199,10 @@ namespace TinkerdinView {
 			// 
 			// btnPlaceDelete
 			// 
-			this->btnPlaceDelete->Location = System::Drawing::Point(413, 186);
+			this->btnPlaceDelete->Location = System::Drawing::Point(310, 151);
+			this->btnPlaceDelete->Margin = System::Windows::Forms::Padding(2);
 			this->btnPlaceDelete->Name = L"btnPlaceDelete";
-			this->btnPlaceDelete->Size = System::Drawing::Size(119, 23);
+			this->btnPlaceDelete->Size = System::Drawing::Size(89, 19);
 			this->btnPlaceDelete->TabIndex = 8;
 			this->btnPlaceDelete->Text = L"Eliminar";
 			this->btnPlaceDelete->UseVisualStyleBackColor = true;
@@ -201,45 +210,51 @@ namespace TinkerdinView {
 			// 
 			// txtPlaceId
 			// 
-			this->txtPlaceId->Location = System::Drawing::Point(192, 38);
+			this->txtPlaceId->Location = System::Drawing::Point(144, 31);
+			this->txtPlaceId->Margin = System::Windows::Forms::Padding(2);
 			this->txtPlaceId->Name = L"txtPlaceId";
-			this->txtPlaceId->Size = System::Drawing::Size(100, 22);
+			this->txtPlaceId->Size = System::Drawing::Size(76, 20);
 			this->txtPlaceId->TabIndex = 9;
 			this->txtPlaceId->TextChanged += gcnew System::EventHandler(this, &PlaceForm::textBox1_TextChanged);
 			// 
 			// txtPlaceAvailability
 			// 
-			this->txtPlaceAvailability->Location = System::Drawing::Point(192, 81);
+			this->txtPlaceAvailability->Location = System::Drawing::Point(144, 66);
+			this->txtPlaceAvailability->Margin = System::Windows::Forms::Padding(2);
 			this->txtPlaceAvailability->Name = L"txtPlaceAvailability";
-			this->txtPlaceAvailability->Size = System::Drawing::Size(100, 22);
+			this->txtPlaceAvailability->Size = System::Drawing::Size(76, 20);
 			this->txtPlaceAvailability->TabIndex = 10;
 			// 
 			// txtPlacePowerOutlet
 			// 
-			this->txtPlacePowerOutlet->Location = System::Drawing::Point(192, 164);
+			this->txtPlacePowerOutlet->Location = System::Drawing::Point(144, 133);
+			this->txtPlacePowerOutlet->Margin = System::Windows::Forms::Padding(2);
 			this->txtPlacePowerOutlet->Name = L"txtPlacePowerOutlet";
-			this->txtPlacePowerOutlet->Size = System::Drawing::Size(100, 22);
+			this->txtPlacePowerOutlet->Size = System::Drawing::Size(76, 20);
 			this->txtPlacePowerOutlet->TabIndex = 12;
 			// 
 			// txtPlaceParticipants
 			// 
-			this->txtPlaceParticipants->Location = System::Drawing::Point(192, 121);
+			this->txtPlaceParticipants->Location = System::Drawing::Point(144, 98);
+			this->txtPlaceParticipants->Margin = System::Windows::Forms::Padding(2);
 			this->txtPlaceParticipants->Name = L"txtPlaceParticipants";
-			this->txtPlaceParticipants->Size = System::Drawing::Size(100, 22);
+			this->txtPlaceParticipants->Size = System::Drawing::Size(76, 20);
 			this->txtPlaceParticipants->TabIndex = 11;
 			// 
 			// txtPlaceLocation
 			// 
-			this->txtPlaceLocation->Location = System::Drawing::Point(192, 243);
+			this->txtPlaceLocation->Location = System::Drawing::Point(144, 197);
+			this->txtPlaceLocation->Margin = System::Windows::Forms::Padding(2);
 			this->txtPlaceLocation->Name = L"txtPlaceLocation";
-			this->txtPlaceLocation->Size = System::Drawing::Size(100, 22);
+			this->txtPlaceLocation->Size = System::Drawing::Size(76, 20);
 			this->txtPlaceLocation->TabIndex = 14;
 			// 
 			// txtPlaceTypePlace
 			// 
-			this->txtPlaceTypePlace->Location = System::Drawing::Point(192, 200);
+			this->txtPlaceTypePlace->Location = System::Drawing::Point(144, 162);
+			this->txtPlaceTypePlace->Margin = System::Windows::Forms::Padding(2);
 			this->txtPlaceTypePlace->Name = L"txtPlaceTypePlace";
-			this->txtPlaceTypePlace->Size = System::Drawing::Size(100, 22);
+			this->txtPlaceTypePlace->Size = System::Drawing::Size(76, 20);
 			this->txtPlaceTypePlace->TabIndex = 13;
 			// 
 			// dgvPlace
@@ -249,12 +264,14 @@ namespace TinkerdinView {
 				this->placeId, this->placeLocation,
 					this->placeAvailability, this->placePowerOutlet
 			});
-			this->dgvPlace->Location = System::Drawing::Point(12, 306);
+			this->dgvPlace->Location = System::Drawing::Point(9, 249);
+			this->dgvPlace->Margin = System::Windows::Forms::Padding(2);
 			this->dgvPlace->Name = L"dgvPlace";
 			this->dgvPlace->RowHeadersWidth = 51;
 			this->dgvPlace->RowTemplate->Height = 24;
-			this->dgvPlace->Size = System::Drawing::Size(549, 241);
+			this->dgvPlace->Size = System::Drawing::Size(412, 196);
 			this->dgvPlace->TabIndex = 15;
+			this->dgvPlace->CellClick += gcnew System::Windows::Forms::DataGridViewCellEventHandler(this, &PlaceForm::dgvPlace_CellClick);
 			this->dgvPlace->CellContentClick += gcnew System::Windows::Forms::DataGridViewCellEventHandler(this, &PlaceForm::dgvPlace_CellContentClick);
 			// 
 			// placeId
@@ -287,9 +304,9 @@ namespace TinkerdinView {
 			// 
 			// PlaceForm
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(576, 559);
+			this->ClientSize = System::Drawing::Size(432, 454);
 			this->Controls->Add(this->dgvPlace);
 			this->Controls->Add(this->txtPlaceLocation);
 			this->Controls->Add(this->txtPlaceTypePlace);
@@ -306,9 +323,9 @@ namespace TinkerdinView {
 			this->Controls->Add(this->label3);
 			this->Controls->Add(this->label2);
 			this->Controls->Add(this->label1);
-			this->Margin = System::Windows::Forms::Padding(4);
 			this->Name = L"PlaceForm";
 			this->Text = L"PlaceForm";
+			this->Load += gcnew System::EventHandler(this, &PlaceForm::PlaceForm_Load);
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dgvPlace))->EndInit();
 			this->ResumeLayout(false);
 			this->PerformLayout();
@@ -391,18 +408,33 @@ private: System::Void btnPlaceDelete_Click(System::Object^ sender, System::Event
 	ShowProducts();
 
 }
-private: System::Void dgvPlace_CellContentClick(System::Object^ sender, System::Windows::Forms::DataGridViewCellEventArgs^ e) {
+private: System::Void dgvPlace_CellClick(System::Object^ sender, System::Windows::Forms::DataGridViewCellEventArgs^ e) {
 
 	int selectedRowIndex = dgvPlace->SelectedCells[0]->RowIndex;
-	int productId = Convert::ToInt32(dgvPlace->Rows[selectedRowIndex]->Cells[0]->Value->ToString());
-	Place^ p = Controller::QueryPlaceById(productId);
-	//txtProductId->Text = "" + p->getId();
+	int placeId = Convert::ToInt32(dgvPlace->Rows[selectedRowIndex]->Cells[0]->Value->ToString());
+	Place^ p = Controller::QueryPlaceById(placeId);
+	//txtPlaceId->Text = "" + p->getId();
 	txtPlaceId->Text = "" + p->Id;//para convertirlo a string
 	txtPlaceAvailability->Text = "" + p->Availability;
 	txtPlaceParticipants->Text = "" + p->Participants;
 	txtPlacePowerOutlet->Text = "" + p->PowerOutlet;
 	txtPlaceTypePlace->Text =  p->TypePlace;
 	txtPlaceLocation->Text =  p->Location;
+	/*if (p->Photo != nullptr) {
+		System::IO::MemoryStream^ ms = gcnew System::IO::MemoryStream(p->Photo);
+		pbPhoto->Image = Image::FromStream(ms);
+	}
+	else {
+		pbPhoto->Image = nullptr;
+		pbPhoto->Invalidate();
+	}*/
+
+
+}
+private: System::Void dgvPlace_CellContentClick(System::Object^ sender, System::Windows::Forms::DataGridViewCellEventArgs^ e) {
+}
+private: System::Void PlaceForm_Load(System::Object^ sender, System::EventArgs^ e) {
+	ShowProducts();
 
 
 }
